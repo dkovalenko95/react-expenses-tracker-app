b516154993092e9e3baa8e4e690ba5596f9f3b25
Dmytro <dkovalenko2425@gmail.com>
2021-12-07 17:32:15 +0200

    date formatted

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
 import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
-
-  // Hardcoded data that shouldn't used:
-  // const expenseDate = new Date(2021, 2,28);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
   // instead of hardcoded data React is using "props" concept.
   return (<div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
@@ -20,7 +22,6 @@ function ExpenseItem(props) {
 
 export default ExpenseItem;
 
-// Components can't just use data stored in other components, BUT instead, we can utilize "props" concept.
 
 // "Props" concept that helps to pass/share data from one component to another by adding a attribute; 
 // "props" make components reusable. We can set properties(props) of our own custom components.
