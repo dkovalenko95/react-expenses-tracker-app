0512ee69253e539d57cac04d262259091299ed27
Dmytro <dkovalenko2425@gmail.com>
2021-12-09 18:34:45 +0200

    1 obj state vs 3 separate states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  // 3 SEPARATE STATES
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+
+  // 1 OBJECT STATE
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: ""
+  });
+
+  const titleChangeHandler = (event) => {
+    // setEnteredTitle(event.target.value);
+    // setUserInput({
+      // ...userInput, /* "..." --> spread operator, used to pull other userInput without loss */
+      // enteredTitle: event.target.value,
+    // })
+    setUserInput((prevState) => {
+      return {
+        ...prevState, 
+        enteredTitle: event.target.value
+      }
+    })
+  }
+  const amountChangeHandler = (event) => {
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput, /* --> spread operator, used to pull other userInput without loss */
+      enteredAmount: event.target.value,
+    })
+  }
+  const dateChangeHandler = (event) => {
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput, /* --> spread operator, used to pull other userInput without loss */
+      enteredDate: event.target.value,
+    })
+  }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} /* value={enteredTitle} */ />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"/>
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} /* value={enteredAmount} */ />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} /* value={enteredDate} */ />
         </div>
       </div>
       <div className="new-expense__actions">
