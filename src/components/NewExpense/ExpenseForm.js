ea809fe411f6c7fa4d2105450d26caf2e05aca14
Dmytro <dkovalenko2425@gmail.com>
2021-12-09 23:54:33 +0200

    two-way binding applied, expenseFormData added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,59 +4,49 @@ import "./ExpenseForm.css";
 
 function ExpenseForm() {
   // 3 SEPARATE STATES
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-
-  // 1 OBJECT STATE
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: ""
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    // setUserInput({
-      // ...userInput, /* "..." --> spread operator, used to pull other userInput without loss */
-      // enteredTitle: event.target.value,
-    // })
-    setUserInput((prevState) => {
-      return {
-        ...prevState, 
-        enteredTitle: event.target.value
-      }
-    })
-  }
+    setEnteredTitle(event.target.value);
+
+  };
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput, /* --> spread operator, used to pull other userInput without loss */
-      enteredAmount: event.target.value,
-    })
-  }
+    setEnteredAmount(event.target.value);
+    
+  };
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput, /* --> spread operator, used to pull other userInput without loss */
-      enteredDate: event.target.value,
-    })
-  }
+    setEnteredDate(event.target.value);
+    
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault(); /* --> method to cancel web-page reloading after form submit */
+
+    const expenseFormData ={
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    console.log(expenseFormData);
+  };
 
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} /* value={enteredTitle} */ />
+          <input type="text" onChange={titleChangeHandler} value={enteredTitle} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} /* value={enteredAmount} */ />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} /* value={enteredDate} */ />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate} />
         </div>
       </div>
       <div className="new-expense__actions">
@@ -64,6 +54,48 @@ function ExpenseForm() {
       </div>
     </form>
   );
-}
+};
 
 export default ExpenseForm;
+
+
+
+// 1 OBJECT STATE
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: ""
+  // });
+
+    //  const titleChangeHandler = (event) => {
+      //  setUserInput({
+      //  ...userInput, /* "..." --> spread operator, used to pull other userInput without loss */
+      //  enteredTitle: event.target.value,
+    //  })
+
+    //  setUserInput((prevState) => {
+      //  return {
+        // ...prevState, 
+        //   enteredTitle: event.target.value
+      //  }
+    //  })
+    //  }
+
+    //  const amountChangeHandler = (event) => {
+      //  setUserInput({
+        //  ...userInput, /* --> spread operator, used to pull other userInput without loss */
+        //  enteredAmount: event.target.value,
+      //  })
+    //  }
+
+    //  const dateChangeHandler = () => {
+      //  setUserInput({
+        //  ...userInput, /* --> spread operator, used to pull other userInput without loss */
+        //  enteredDate: event.target.value,
+      //  })
+    //  }
+    
+
+
+// 'TWO-WAY BINDING' means that for inputs we don't just listen to changes, but we can also pass a new value back into the input. 
+// So that we can reset or change the input programmatically. All we have to do is add the value attribute, which is a default attribute, to this input element. This will set the internal value property, which every input element has. And we can set it to a new value.
