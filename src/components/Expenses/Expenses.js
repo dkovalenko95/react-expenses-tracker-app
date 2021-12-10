f49a245cd5ca42ec910c45e34b12dc45113efa36
Dmytro <dkovalenko2425@gmail.com>
2021-12-10 14:28:20 +0200

    ExpensesFilter created

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 import "./Expenses.css";
 
+
 function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const checkFilterHandler = (settedYear) => {
+    setSelectedYear(settedYear)
+
+    console.log(selectedYear);
+    console.log(settedYear); // settedYear is a parameter we recieved from filter values
+  }
+
   return (
     <Card className="expenses">
+
+      <ExpensesFilter onCheckFilter={checkFilterHandler} selectedValue={selectedYear} />
+       {/* ExpensesFilter is a controlled component --> whenever we use two-way binding - we are controlling component --> the value which we used in component, like the value we selected in dropdown in ExpenseFilter.js(select tag) is passed on to a parent(from ExpensesFilter to Expenses) component through props and is recieved from the parent component
+      
+      --- ExpensesFilter just represents the dropdown and then attaches a couple of listeners or props
+      --- the real logic recites in the parent component(Expenses) and that turns ExpensesFilter into something which is called a controlled component.
+      
+      That means both the VALUE, as well as CHANGES to the value are not handled in the component itself(ExpensesFilter) but in a parent(Expenses) component. Expenses component controls the ExpensesFilter component.*/}
+
       <ExpenseItem 
         title={props.elements[0].title}
         amount={props.elements[0].amount}
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -10,7 +10,7 @@ function NewExpense(props) {
       ...enteredExpenseData, /* --> "enteredExpenseData" is a value that contains in expenseFormData from ExpenseForm */ 
       id: Math.random().toString()
     };
-    props.onAddExpense(expenseData); /* --> same concept as in ExpenseForm comp(line 33) */
+    props.onAddExpense(expenseData); /* --> same concept as in ExpenseForm comp(line 33); there also a concept named "LIFTING-STATE-UP" - we're lifting up "expenseData" to the App.js comp, so after we could pass it to Expense comp; LIFTING-STATE-UP - moving data from child comp to some parent comp to to either use it there or to then pass it down to some other child comp. */
 
     console.log(expenseData);
     console.log(enteredExpenseData);
