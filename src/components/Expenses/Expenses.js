02b022c1ad64c1e1f43ac8c236f0bd4d19e2a7a3
Dmytro <dkovalenko2425@gmail.com>
2021-12-11 20:17:40 +0200

    filter applied

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,20 +11,27 @@ function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2021");
 
   const checkFilterHandler = (settedYear) => {
-    setSelectedYear(settedYear)
+    setSelectedYear(settedYear);
 
     console.log(selectedYear);
     console.log(settedYear); // settedYear is a parameter we recieved from filter values
   }
 
+  const filterExpenses = props.elements.filter(expense => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
     <Card className="expenses">
 
-      <ExpensesFilter onCheckFilter={checkFilterHandler} selectedValue={selectedYear} />
+      <ExpensesFilter 
+        onCheckFilter={checkFilterHandler} 
+        selectedValue={selectedYear} 
+      />
 
       {/* DYNAMIC rendering one ExpenseItem per element in the array */}
       {/* single curly braces "{}" - means the executing the dynamic expression(where JS expressions can be executed) in JSX code */}
-      {props.elements.map((expenseElem, index) => (
+      {filterExpenses.map((expenseElem) => (
         <ExpenseItem
           key={expenseElem.id} /* key - is a prop, some sort of identifier */
           title={expenseElem.title}
